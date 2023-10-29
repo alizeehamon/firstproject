d934aaece9b89403b65e01891fe94b0681c5981d
Alizée <alizee.hamon@laposte.net>
2023-10-29 15:30:52 +0100

    interceptor

--- src/app/core/log.interceptor.ts
+++ src/app/core/log.interceptor.ts
@@ -7,10 +7,14 @@ export class LogInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler):  Observable<HttpEvent<any>> {
     let start = new Date().getTime();
+    console.log('URL appelé : ', req.url)
     return next.handle(req).pipe(
       tap((response) => {
+        if (response instanceof HttpResponse) {
           console.log("Temps écoulé de la requête (en ms):")
           console.log(new Date().getTime() - start)
+          console.log('Réponse:', response.body);
+        }
         }))
   }
 }
