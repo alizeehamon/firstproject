a22dc0d55edba41d7262129133be7760e3f7f2b0
Alizée <alizee.hamon@laposte.net>
2023-10-29 15:07:57 +0100

    quest done

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { User } from '../models/user.model';
 
 @Component({
@@ -8,29 +8,29 @@ import { User } from '../models/user.model';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  user: User | null = null;
+  user?: User;
 
-  username = new FormControl('', Validators.required);
-  email = new FormControl('', Validators.email);
-  password = new FormControl('', Validators.required);
-  street = new FormControl('', Validators.required);
-  postCode = new FormControl('', Validators.required);
-  city = new FormControl('', Validators.required);
+  userForm = this.fb.group({
+  username: new FormControl('', Validators.required),
+  credentials:this.fb.group({
+  email: new FormControl('', Validators.email),
+  password: new FormControl('', Validators.required),
+  }),
+  address:this.fb.group({
+    street: new FormControl('', Validators.required),
+    postCode: new FormControl('', Validators.required),
+    city: new FormControl('', Validators.required),
+  })
+  })
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
 
   onSubmit() {
-    this.user = new User(
-      this.username.value ?? '',
-      this.email.value ?? '',
-      this.password.value ?? '',
-      this.street.value ?? '',
-      this.postCode.value ?? '',
-      this.city.value ?? '',
-    );
+    console.log(this.userForm.value);
+    this.user = new User(this.userForm.value.username?? '', this.userForm.value.credentials?.email?? '', this.userForm.value.credentials?.password?? '', this.userForm.value.address?.street?? '', this.userForm.value.address?.postCode?? '', this.userForm.value.address?.street?? '');
   }
 }
